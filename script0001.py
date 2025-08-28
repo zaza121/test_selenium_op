e8f70c9be6825879a88e64c56dbd6a1daaad2b2a
mr zaza121 <hamza.yaya121@gmail.com>
2025-08-28 16:20:16 +0100

    Update script0001.py

--- script0001.py
+++ script0001.py
@@ -14,7 +14,24 @@ driver = Remote(command_executor=grid_url, options=chrome_options)
 
 try:
     # Open a website
-    driver.get("https://www.python.org")
+    # driver.get("https://www.python.org")
+    driver.get("http://146.59.110.97:8083/web/database/selector")
+    driver.set_window_size(602, 743)
+    driver.implicitly_wait(8)
+
+    # select database
+    driver.find_element(By.LINK_TEXT, "21_07_2025").click()
+    driver.implicitly_wait(5)
+
+    # set credentials
+    driver.find_element(By.ID, "login").click()
+    driver.find_element(By.ID, "login").send_keys("contact@sammi.mc")
+    driver.find_element(By.CSS_SELECTOR, ".field-login").click()
+    driver.find_element(By.ID, "password").click()
+    driver.find_element(By.ID, "password").send_keys("odoo2023!")
+    driver.find_element(By.CSS_SELECTOR, ".mb-3:nth-child(4)").click()
+    driver.find_element(By.CSS_SELECTOR, ".btn-primary").click()
+    driver.implicitly_wait(10)
 
     # Print the page title
     print(f"Page Title: {driver.title}")
@@ -35,4 +52,5 @@ try:
 
 finally:
     # Close the browser
+    driver.quit()
     driver.quit()
